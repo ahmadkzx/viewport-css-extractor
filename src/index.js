97c33fb34f626414f1b5fa2a759342d05cb92e78
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-01-02 22:29:53 +0330

    refactor: beautify code

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const puppeteer = require('puppeteer');
 
 (async function() {
   try {
-    const viewportCssRules = await getViewportCss()
-    await createCssFile(viewportCssRules)
+    const test = await getViewportCss('https://test.com', 200)
+    createCssFile(test)
 
   } catch(err) {
     console.error(err)
@@ -17,15 +17,21 @@ const puppeteer = require('puppeteer');
  * extract viewport elements and their styles in string type
  * @returns {Promise}
  */
-function getViewportCss() {
+function getViewportCss(url, viewportBottom = 1600) {
   return new Promise(async (resolve, reject) => {
     try {
+      /**
+       * open virtual browser (chromium)
+       */
       const browser = await puppeteer.launch()
       const page = await browser.newPage()
-      await page.goto('http://127.0.0.1:5500/mock/index.html', { waitUntil: 'domcontentloaded', timeout: 20000 })
-  
-      const viewportBottom = 3000
+      await page.goto(url, { waitUntil: 'networkidle0', timeout: 50000 })
 
+
+      /**
+       * download all css files with axios
+       * merge them into one string
+       */
       let allCssString = ''
       const styleSheetUrls = await page.evaluate(() => {
         const styleSheets = Array.from(document.styleSheets).filter(sheet => !!sheet.href)
@@ -37,11 +43,21 @@ function getViewportCss() {
         allCssString = res.map(res => res.data + ' ')
       })
 
+
+      /**
+       * get viewport elements
+       * create new stylesheet using merged css string
+       * extract viewport css
+       * @returns {string} - viewport css string
+       */
       const viewportCssString = await page.evaluate((allCssString, viewportBottom) => {
         let output = ''
 
         const elements = Array.from(document.getElementsByTagName('*'))
-        const viewportElements = elements.filter(el => el.offsetTop <= viewportBottom)
+        const viewportElements = elements.filter(el => {
+          const offsetTop = window.scrollY + el.getBoundingClientRect().top
+          return offsetTop <= viewportBottom
+        })
 
         const styleSheet = new CSSStyleSheet()
         styleSheet.replaceSync(allCssString)
@@ -49,8 +65,6 @@ function getViewportCss() {
         const allCssRules = Array.from(styleSheet.cssRules)
 
         viewportElements.map(el => {
-          el.matches = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector || el.oMatchesSelector
-
           allCssRules.map(rule => {
             if (el.matches(rule.selectorText)) output = `${output} ${rule.cssText}`
           })
@@ -61,7 +75,6 @@ function getViewportCss() {
       }, allCssString, viewportBottom)
 
       await browser.close()
-
       resolve(viewportCssString)
 
     } catch(err) {
@@ -69,6 +82,7 @@ function getViewportCss() {
     }
   })
 }
+
 
 function createCssFile(css) {
   return new Promise(async (resolve, reject) => {
